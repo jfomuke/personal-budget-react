f10ba3baaa8b948895c847524aa84a176796d0d6
Jason Fomuke <jfomuke@uncc.edu>
2020-10-15 13:45:22 -0400

    Coding Exercise 03 Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './App.css';
+// Axios Import
+// import 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js' 
+// import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js'
+import axios from 'axios';
+import "./info.json";
+import Chart from "chart.js";
+
+
 
 import {
   BrowserRouter as Router,
@@ -20,7 +28,6 @@ function App() {
   return (
 
     <Router>
-      
       <Menu/>
       <Hero/>
         <div className='mainContainer'>
@@ -44,5 +51,56 @@ function App() {
     </Router>
   );
 }
+
+// var dataSource = require('./info.json');
+
+var dataSource = {
+  datasets: [
+      {
+          data: [],
+          backgroundColor: [
+              '#ffcd56',
+              '#ff6384',
+              '#36a2eb',
+              '#aa7b19',
+              '#fffb19',
+              '#bbab19',
+              '#dd6b19'
+          ],
+      }
+  ],
+  labels: [
+      
+  ]
+};
+
+// Axios Code
+function getBudget() 
+        {
+            axios.get('http://localhost:3000/info.json' ).then(function (res) 
+            {
+                //console.log(res);
+                for (var i = 0; i < res.data.budget.myBudget.length; i++) 
+                {
+                  console.log(dataSource)
+                    dataSource.datasets[0].data[i] = res.data.budget.myBudget[i].budget;
+                    dataSource.labels[i] = res.data.budget.myBudget[i].title;
+                }
+                createChart();
+            });
+        }
+
+
+function createChart() 
+{
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myPieChart = new Chart(ctx, 
+    {
+        type: 'pie',
+        data: dataSource
+    });
+}
+
+getBudget()
 
 export default App;
